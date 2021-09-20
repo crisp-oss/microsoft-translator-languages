d140544b8e5a5675c028cf9276e7ef91808bb05a
Eliott Vincent <dev@eliottvincent.com>
2021-09-20 16:58:53 +0200

    Initialize index

---
+++ src/index.js
@@ -0,0 +1,72 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+/**************************************************************************
+ * IMPORTS
+ ***************************************************************************/
+
+var fs = require("fs");
+var path = require("path");
+
+var dataDir = path.join(__dirname, "/../data/");
+
+var dataFiles = {
+  translation: path.join(dataDir, "translation.json"),
+  transliteration: path.join(dataDir, "transliteration.json"),
+  dictionary: path.join(dataDir, "dictionary.json")
+};
+
+/**************************************************************************
+ * FUNCTIONS
+ ***************************************************************************/
+
+var data = {
+  translation: {},
+  transliteration: {},
+  dictionary: {}
+};
+
+var load = function() {
+  for (var fileName in dataFiles) {
+    var buffer = fs.readFileSync(dataFiles[fileName]);
+
+    data[fileName] = (
+      JSON.parse(buffer)
+    );
+  }
+};
+
+var isLanguageSupported = function(code, scope) {
+  if (!code || !scope) {
+    throw new Error("Missing code and/or scope");
+  }
+
+  if (data[scope] === undefined || data[scope] === {}) {
+    return false;
+  }
+
+  return Object.prototype.hasOwnProperty.call(data[scope], code);
+};
+
+load();
+
+/**************************************************************************
+ * EXPORTS
+ ***************************************************************************/
+
+exports.isLanguageSupported = isLanguageSupported;
+
+exports.translation = data.translation;
+exports.transliteration = data.transliteration;
+exports.dictionary = data.dictionary;
+
+exports.default = {
+  isLanguageSupported: isLanguageSupported,
+
+  dataTranslation: data.translation,
+  dataTransliteration: data.transliteration,
+  dataDictionary: data.dictionary
+};
