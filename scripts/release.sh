ed0c40e2225121d7f0c5b256636e007458ca22a3
Eliott Vincent <dev@eliottvincent.com>
2021-09-21 14:55:39 +0200

    Test release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,21 @@
-VER=`node -p "require('./package.json').version"`
+#!/bin/bash
 
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
-git commit -a -m "Auto Release v${VER}"
-git tag "v${VER}" HEAD
+# VER=`node -p "require('./package.json').version"`
+#
+# git config --local user.email "action@github.com"
+# git config --local user.name "GitHub Action"
+# git commit -a -m "Auto Release v${VER}"
+# git tag "v${VER}" HEAD
+
+echo "${env.INPUT_GITHUB_TOKEN}"
+echo "${process.env.INPUT_GITHUB_TOKEN}"
+
+echo "${env.INPUT_TEST}"
+echo "${process.env.INPUT_TEST}"
+#
+# remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+#
+# git push "${remote_repo}" \
+#   HEAD:master \
+#   --follow-tags \
+#   --tags
