785f508652201b09d9f580974516f9412d496c7e
Eliott Vincent <dev@eliottvincent.com>
2021-09-21 15:23:22 +0200

    Env var

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,11 +7,12 @@
 # git commit -a -m "Auto Release v${VER}"
 # git tag "v${VER}" HEAD
 
-echo "${env.INPUT_GITHUB_TOKEN}"
-echo "${process.env.INPUT_GITHUB_TOKEN}"
-
-echo "${env.INPUT_TEST}"
-echo "${process.env.INPUT_TEST}"
+echo $INPUT_GITHUB_TOKEN
+echo $env.INPUT_GITHUB_TOKEN
+echo $INPUT_TEST
+echo $env.INPUT_TEST
+echo $process.env.INPUT_GITHUB_TOKEN
+echo $process.env.INPUT_TEST
 #
 # remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 #
