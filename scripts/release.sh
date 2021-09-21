09dd99a7bb0d6cacd7beffcded68fd8ac85c77ef
Eliott Vincent <dev@eliottvincent.com>
2021-09-21 15:33:32 +0200

    Release should work

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# VER=`node -p "require('./package.json').version"`
-#
-# git config --local user.email "action@github.com"
-# git config --local user.name "GitHub Action"
-# git commit -a -m "Auto Release v${VER}"
-# git tag "v${VER}" HEAD
+VER=`node -p "require('./package.json').version"`
 
-echo $GITHUB_TOKEN
-echo $TEST
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git commit -a -m "Auto Release v${VER}"
+git tag "v${VER}" HEAD
 
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-echo $remote_repo
-
-# git push "${remote_repo}" \
-#   HEAD:master \
-#   --follow-tags \
-#   --tags
+git push "${remote_repo}" \
+  HEAD:master \
+  --follow-tags \
+  --tags
